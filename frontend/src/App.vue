1c690ef5b6078357f396d8fed3d040474b4e8913
Jaden Jin <Jadenjin12345@gmail.com>
2023-05-29 17:33:59 +0100

    Responsive resizing on sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,67 +5,71 @@ import { useAuthenticationComponentStore } from "@/stores/AuthenticationComponen
 
 const authenticationStore = useAuthenticationComponentStore();
 
-const { isAuthenticationComponentOpen, isSignUpComponentOpen, isAccountDetailsComponentOpen } = storeToRefs(
-  useAuthenticationComponentStore()
-);
+const {
+  isAuthenticationComponentOpen,
+  isSignUpComponentOpen,
+  isAccountDetailsComponentOpen,
+} = storeToRefs(useAuthenticationComponentStore());
 </script>
 
 <template>
   <div class="w-full h-full mx-auto">
-    <AuthenticationComponent v-if="isAuthenticationComponentOpen" class="fixed z-20"/>
-    <SignUpComponent v-if="isSignUpComponentOpen" class="fixed z-20"/>
-    <AccountDetailsComponent v-if="isAccountDetailsComponentOpen" class="fixed z-20"/>
+    <AuthenticationComponent
+      v-if="isAuthenticationComponentOpen"
+      class="fixed z-20"
+    />
+    <SignUpComponent v-if="isSignUpComponentOpen" class="fixed z-20" />
+    <AccountDetailsComponent
+      v-if="isAccountDetailsComponentOpen"
+      class="fixed z-20"
+    />
 
-
-    <div class="bg-white rounded-[15px] dropShadow px-[32px] py-[20px] h-min sticky top-0 z-10">
+    <div
+      class="bg-white rounded-[15px] dropShadow px-[32px] py-[20px] h-min sticky top-0 z-10"
+    >
       <NavBarComponent />
     </div>
     <div class="flex">
       <div class="w-[22%]">
-        <div class="justify-between p-[32px] pl-[64px] h-min sticky top-[74px]">
+        <div class="justify-between p-[32px] h-min sticky top-[74px]">
           <SidebarComponent />
         </div>
       </div>
       <div class="w-[56%] p-[32px]">
-        <router-view/>
+        <router-view />
       </div>
-      <div class="w-[22%] p-[32px] pr-[64px] space-y-[32px] h-min sticky top-[74px]">
+      <div class="w-[22%] p-[32px] space-y-[32px] h-min sticky top-[74px]">
         <NotificationComponent />
         <FriendsComponent />
       </div>
     </div>
-
-
-
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+import { defineComponent } from "vue";
 
-  import NavBarComponent from './components/NavBarComponent.vue';
-  import NotificationComponent from './components/NotificationComponent.vue';
-  import FriendsComponent from './components/FriendsComponent.vue';
-  import SidebarComponent from './components/SidebarComponent.vue';
-  import AuthenticationComponent from './components/AuthenticationComponent.vue'
-  import SignUpComponent from './components/SignUpComponent.vue';
-  import AccountDetailsComponent from "@/components/AccountDetailsComponent.vue";
+import NavBarComponent from "./components/NavBarComponent.vue";
+import NotificationComponent from "./components/NotificationComponent.vue";
+import FriendsComponent from "./components/FriendsComponent.vue";
+import SidebarComponent from "./components/SidebarComponent.vue";
+import AuthenticationComponent from "./components/AuthenticationComponent.vue";
+import SignUpComponent from "./components/SignUpComponent.vue";
+import AccountDetailsComponent from "@/components/AccountDetailsComponent.vue";
 
+export default defineComponent({
+  name: "App",
+  components: {
+    SidebarComponent,
+    NavBarComponent,
+    NotificationComponent,
+    FriendsComponent,
 
-
-  export default defineComponent({
-    name: "App",
-    components: {
-      SidebarComponent,
-      NavBarComponent,
-      NotificationComponent,
-      FriendsComponent,
-
-      AuthenticationComponent,
-      SignUpComponent,
-      AccountDetailsComponent
-    },
-  })
+    AuthenticationComponent,
+    SignUpComponent,
+    AccountDetailsComponent,
+  },
+});
 </script>
 
 <style lang="scss">
@@ -73,50 +77,57 @@ const { isAuthenticationComponentOpen, isSignUpComponentOpen, isAccountDetailsCo
 @tailwind components;
 @tailwind utilities;
 
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 
 .dropShadow {
-  box-shadow: 0px 26px 80px rgba(26, 34, 35, 0.07), 0px 10.8622px 33.4221px rgba(26, 34, 35, 0.0503198), 0px 5.80744px 17.869px rgba(26, 34, 35, 0.0417275), 0px 3.2556px 10.0172px rgba(26, 34, 35, 0.035), 0px 1.72903px 5.32008px rgba(26, 34, 35, 0.0282725), 0px 0.719487px 2.21381px rgba(26, 34, 35, 0.0196802);
+  box-shadow: 0px 26px 80px rgba(26, 34, 35, 0.07),
+    0px 10.8622px 33.4221px rgba(26, 34, 35, 0.0503198),
+    0px 5.80744px 17.869px rgba(26, 34, 35, 0.0417275),
+    0px 3.2556px 10.0172px rgba(26, 34, 35, 0.035),
+    0px 1.72903px 5.32008px rgba(26, 34, 35, 0.0282725),
+    0px 0.719487px 2.21381px rgba(26, 34, 35, 0.0196802);
 }
 
-@layer base{
+@layer base {
   p {
     font-family: Roboto, sans-serif;
-    @apply font-normal text-[14px] leading-[17px] text-black-100;
+    @apply font-normal text-[14px] text-black-100;
   }
 
   textarea {
     font-family: Roboto, sans-serif;
-    @apply font-normal text-[16px] leading-[17px] text-black-100;
+    @apply font-normal text-[16px] text-black-100;
   }
 
   h1 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[43px] leading-[61px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[43px] text-black-100;
   }
 
   h2 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[34px] leading-[49px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[34px] text-black-100;
   }
 
   h3 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[27px] leading-[39px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[27px] text-black-100;
   }
 
   h4 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[22px] leading-[31px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[22px] text-black-100;
   }
 
   h5 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[18px] leading-[26px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[18px] text-black-100;
   }
 
   h6 {
-    font-family: Brandon Grotesque, sans-serif;
-    @apply font-bold text-[14px] leading-[20px] text-black-100;
+    font-family: Lato, sans-serif;
+    @apply font-bold text-[14px] text-black-100;
   }
-};
+} ;
 </style>
--- frontend/src/components/SidebarComponent.vue
+++ frontend/src/components/SidebarComponent.vue
@@ -1,42 +1,66 @@
 <template>
-  <div class="w-full">
+  <div class="2xl:w-full xl:w-full lg:w-[64px] md:w-[64px] sm:w-[64px]">
     <nav class="bg-white rounded-[25px] py-[32px] space-y-[8px] dropShadow">
-      <router-link to="/" :class="{dropShadow: $route.name==='home'}" class="sidebarItem">
+      <router-link
+        to="/"
+        :class="{ dropShadow: $route.name === 'home' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'home'" class="highlightBar"></div>
-        <i class="fa-solid fa-house text-[24px] mr-[16px]"></i>
-        Home
+        <i class="fa-solid fa-house icon"></i>
+        <h1 class="sidebarText">Home</h1>
       </router-link>
-      <router-link to="/favourites" :class="{dropShadow: $route.name==='favourites'}" class="sidebarItem">
+      <router-link
+        to="/favourites"
+        :class="{ dropShadow: $route.name === 'favourites' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'favourites'" class="highlightBar"></div>
-        <i class="fa-solid fa-star text-[24px] mr-[16px]"></i>
-        Favourites
+        <i class="fa-solid fa-star icon"></i>
+        <h1 class="sidebarText">Favourites</h1>
       </router-link>
-      <router-link to="/messages" :class="{dropShadow: $route.name==='messages'}" class="sidebarItem">
+      <router-link
+        to="/messages"
+        :class="{ dropShadow: $route.name === 'messages' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'messages'" class="highlightBar"></div>
-        <i class="fa-solid fa-message text-[24px] mr-[16px]"></i>
-        Messages
+        <i class="fa-solid fa-message icon"></i>
+        <h1 class="sidebarText">Messages</h1>
       </router-link>
-      <router-link to="/friends" :class="{dropShadow: $route.name==='friends'}" class="sidebarItem">
+      <router-link
+        to="/friends"
+        :class="{ dropShadow: $route.name === 'friends' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'friends'" class="highlightBar"></div>
         <i class="fa-solid fa-user-group text-[24px] mr-[8px]"></i>
-        Friends
+        <h1 class="sidebarText">Friends</h1>
       </router-link>
-      <router-link to="/notifications" :class="{dropShadow: $route.name==='notifications'}" class="sidebarItem">
+      <router-link
+        to="/notifications"
+        :class="{ dropShadow: $route.name === 'notifications' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'notifications'" class="highlightBar"></div>
-        <i class="fa-solid fa-bell text-[24px] mr-[16px]"></i>
-        Notifications
+        <i class="fa-solid fa-bell icon"></i>
+        <h1 class="sidebarText">Notifications</h1>
       </router-link>
-      <router-link to="/settings" :class="{dropShadow: $route.name==='settings'}" class="sidebarItem">
+      <router-link
+        to="/settings"
+        :class="{ dropShadow: $route.name === 'settings' }"
+        class="sidebarItem"
+      >
         <div v-if="$route.name === 'settings'" class="highlightBar"></div>
-        <i class="fa-solid fa-gear text-[24px] mr-[16px]"></i>
-        Settings
+        <i class="fa-solid fa-gear icon"></i>
+        <h1 class="sidebarText">Settings</h1>
       </router-link>
     </nav>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "SidebarComponent",
@@ -48,11 +72,20 @@ export default defineComponent({
 
 @layer components {
   .sidebarItem {
-    @apply flex w-full py-[12px] px-[16px] font-['brandon-grotesque'] font-[500] text-black-100 leading-[0px] text-[28px] hover:bg-black-10 rounded-[10px] items-center
+    @apply flex w-full py-[12px] px-[16px] hover:bg-black-10 rounded-[10px] items-center overflow-hidden;
   }
 
   .highlightBar {
-    @apply h-[28px] w-[3px] ml-[-16px] mr-[13px] bg-primary-100 rounded-full
+    @apply h-[28px] w-[3px] ml-[-16px] mr-[13px] bg-primary-100 rounded-full;
+  }
+
+  .icon {
+    @apply text-[24px] 2xl:mr-[16px] xl:mr-[16px];
+  }
+
+  .sidebarText {
+    font-family: "Lato", sans-serif;
+    @apply font-[400] text-black-100 text-[28px] leading-none hidden 2xl:block xl:block;
   }
 }
 </style>
